da3a1f9b367e174af17b1becd5a64ed2f82a456a
Dreaxxx <ldqtheone@gmail.com>
2020-11-10 11:38:20 +0100

    push 27

--- requetes.js
+++ requetes.js
@@ -31,6 +31,19 @@ db.users.remove( { name: 'Brian' }, true )
 Requete 11:
 db.users.updateMany({occupation: 'programmer'}, {$set: { occupation: 'developper' } } )
 
+Requete 12:
+db.movies.find(
+    { title: { $regex: /198.*/ } }
+).count()
+
+Requete 13:
+db.movies.find(
+    { genres: { $regex: /Horror/, $options:'i'}}
+).count()
+
+Requete 14:
+db.movies.find({ $or: [ { title: { $regex: /(198[4-9])/i } }, { title: { $regex: /(199[0-2])/i } } ] }).count()
+
 Requete 16:
 var t = db.movies.find();
 t.forEach(function(fields){
@@ -72,3 +85,56 @@ db.users.update(
 )
 
 Requete 19:
+db.users.find({'movies.movieid': 1196 }, {name: 1}).count()
+
+Requete 20:
+db.users.find(
+    {
+        'movies.movieid': { $all: [1196, 260, 1210 ]}
+    }).count()
+
+Requete 21:
+db.users.find({movies : { $size: 48 }}).count()
+
+Requete 22:
+var d = db.users.find();
+d.forEach(function(users){
+    var numRating = users.movies.length;
+    db.users.update({name: users.name}, {$set: { num_rating: numRating }})
+})
+
+Requete 23:
+db.users.find({ $where: 'this.movies.length > 90' }).count()
+db.users.find({num_rating: { $gt: 90}}).count()
+
+Requete 24:
+db.users.find({'movies.timestamp' : { $gte:  (978307200 * 1000)}}, {'movies.timestamp': 1}).count()
+db.users.aggregate([{ $project : { timestamp: "$movies.timestamp"   , name: 1, qty:1} }, {$match:{timestamp: {$elemMatch:{$gte : new Date('01/01/2001').getTime()/1000}} } }, {$group:{_id:"1", total :{$sum: {$size: "$timestamp"}}} }]);
+
+Requete 25:
+db.users.find({name: 'Jayson Brad'}, {name:1, movies : 1, movies: {$slice: -3}}).sort({'movies.timestamp': -1})
+
+Requete 26:
+db.users.find({name: 'Tracy Edward'}, {occupation: 1, age:1, name:1, movies : {$elemMatch: {movieid: 1210}}})
+
+Requete 27:
+db.movies.aggregate([
+    {
+        $match:{'title': "Untouchables, The"}
+    },
+    {
+        $lookup:
+            {
+                from: 'users',
+                pipeline: [
+                    { $match: { 'movies.rating': { $elemMatch: {$gte : 5 }}} },
+                ],
+                as: "filmId"
+            }
+    },
+    {
+        $count: "Count"
+    }
+])
+
+
